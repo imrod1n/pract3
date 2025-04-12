3c1d0d93ce1e68f3c8ab5a68ccbe62b2a0577e5c
imrod1n <imrod1n@yandex.ru>
2025-04-12 11:58:20 +0300

    fixed task 4

--- task4/index.js
+++ task4/index.js
@@ -4,42 +4,42 @@ const people = [{
     profession: 'mathematician',
     accomplishment: 'spaceflight calculations',
     imageId: 'MK3eW3A'
-  }, {
+}, {
     id: 1, // Used in JSX as a key
     name: 'Mario José Molina-Pasquel Henríquez',
     profession: 'chemist',
     accomplishment: 'discovery of Arctic ozone hole',
     imageId: 'mynHUSa'
-  }, {
+}, {
     id: 2, // Used in JSX as a key
     name: 'Mohammad Abdus Salam',
     profession: 'physicist',
     accomplishment: 'electromagnetism theory',
     imageId: 'bE7W1ji'
-  }, {
+}, {
     id: 3, // Used in JSX as a key
     name: 'Percy Lavon Julian',
     profession: 'chemist',
     accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
     imageId: 'IOjWm71'
-  }, {
+}, {
     id: 4, // Used in JSX as a key
     name: 'Subrahmanyan Chandrasekhar',
     profession: 'astrophysicist',
     accomplishment: 'white dwarf star mass calculations',
     imageId: 'lrWQx8l'
-  }];
+}];
   
-  function getImageUrl(person) {
+function getImageUrl(person) {
     return (
-      'https://i.imgur.com/' +
-      person.imageId +
-      's.jpg'
+        'https://i.imgur.com/' +
+        person.imageId +
+        's.jpg'
     );
-  }
+}
 
-  const sortArrayByProperty = (array, property = 'name', order = 'asc') => {
-    return array.sort((a, b) => {
+const sortArray = (array, property = 'name', order = 'asc') => {
+    let sort_list = array.sort((a, b) => {
         if (a[property] < b[property]) {
             return order === 'asc' ? -1 : 1;
         }
@@ -47,35 +47,37 @@ const people = [{
             return order === 'asc' ? 1 : -1;
         }
     });
+    ReactDOM.render(List(sort_list), document.getElementById("app"));
 };
 
-  const filterArray = (array, property, value) => {
-    if (value === '' || property === '') return array;
-    else return array.filter(person => person[property] === value); 
-  }
+const filterArray = (array, property, value) => {
+    if (value === '' || property === '') ReactDOM.render(List(array), document.getElementById("app"));
+    else {
+        let filt_list = array.filter(person => person[property] === value);
+        ReactDOM.render(List(filt_list), document.getElementById("app"));
+    }
+}
 
-function List(array, sort_property, filt_property, order, value) {
-const filt_list = filterArray(array, filt_property, value);
-const sort_list = sortArrayByProperty(filt_list, sort_property, order);
+function List(array) {
 
-  const listItems = sort_list.map(person =>
-    <tr key={person.id}>
-        <td>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-        </td>
-        <td>
-            <b>{person.name}</b>
-        </td>
-        <td>
+    const listItems = array.map(person =>
+        <tr key={person.id}>
+            <td>
+                <img
+                    src={getImageUrl(person)}
+                    alt={person.name}
+                />
+            </td>
+            <td>
+                <b>{person.name}</b>
+            </td>
+            <td>
             {' ' + person.profession + ' '}
             known for {person.accomplishment}
-        </td>
-    </tr>
-  );
-  return listItems;
+            </td>
+        </tr>
+    );
+    return listItems;
 }
 
 ReactDOM.render(List(people, "name" , "", "asc", ""),document.getElementById("app"));
